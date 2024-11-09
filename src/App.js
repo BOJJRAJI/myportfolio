55ca2cbfcc0cb840a1fd52719d720f1d48274b11
raji <johndoe@example.com>
2024-11-09 15:24:23 +0530

    CHANGES

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         HI LEKYA 
+         HOW ARE YOU ?
         </a>
       </header>
     </div>
